92c97dff2308ff325a1a8ebb4dbe1b38c9851637
PickleFace5 <thesavagedemon7@gmail.com>
2025-03-18 19:02:43 -0700

    Target pose only changes when the bumper is initially pressed

--- robot_container.py
+++ robot_container.py
@@ -19,6 +19,7 @@ from subsystems.funnel import FunnelSubsystem
 from subsystems.intake import IntakeSubsystem
 from subsystems.pivot import PivotSubsystem
 from subsystems.superstructure import Superstructure
+from subsystems.swerve import DriverAssist
 from subsystems.vision import VisionSubsystem
 
 
@@ -112,8 +113,17 @@ class RobotContainer:
         common_settings: Callable[[swerve.requests.SwerveRequest], swerve.requests.SwerveRequest] = lambda req: req.with_deadband(self._max_speed * 0.01).with_rotational_deadband(self._max_angular_rate * 0.01).with_drive_request_type(
             swerve.SwerveModule.DriveRequestType.VELOCITY
         ).with_steer_request_type(swerve.SwerveModule.SteerRequestType.MOTION_MAGIC_EXPO)
-        self._field_centric = common_settings(swerve.requests.FieldCentric())
-        self._robot_centric = common_settings(swerve.requests.RobotCentric())
+        self._field_centric: swerve.requests.FieldCentric = common_settings(swerve.requests.FieldCentric())
+        self._robot_centric: swerve.requests.RobotCentric = common_settings(swerve.requests.RobotCentric())
+
+        self._driver_assist: DriverAssist = common_settings(
+            DriverAssist()
+            .with_translation_pid(Constants.AutoAlignConstants.TRANSLATION_P, Constants.AutoAlignConstants.TRANSLATION_I, Constants.AutoAlignConstants.TRANSLATION_D)
+            .with_heading_pid(Constants.AutoAlignConstants.HEADING_P, Constants.AutoAlignConstants.HEADING_I, Constants.AutoAlignConstants.HEADING_D)
+            .with_max_distance(Constants.AutoAlignConstants.MAX_DISTANCE)
+            .with_elevator_up_function(lambda: not self.elevator.get_current_state() == self.elevator.SubsystemState.DEFAULT)
+            )
+        
         self._brake = swerve.requests.SwerveDriveBrake()
         self._point = swerve.requests.PointWheelsAt()
 
@@ -158,7 +168,30 @@ class RobotContainer:
             )
         )
 
-        self._driver_controller.leftBumper().onTrue(self.drivetrain.runOnce(lambda: self.drivetrain.seed_field_centric()))
+        self._driver_controller.leftBumper().whileTrue(
+            self.drivetrain.apply_request_once(
+                lambda: self._driver_assist
+                .with_velocity_x(-hid.getLeftY() * self._max_speed)
+                .with_velocity_y(-hid.getLeftX() * self._max_speed)
+                .with_rotational_rate(-self._driver_controller.getRightX() * self._max_angular_rate)
+                .with_fallback(self._field_centric)
+                .with_change_target_pose(True)
+                .with_branch_side(DriverAssist.BranchSide.LEFT)
+            )
+            .andThen(
+                self.drivetrain.apply_request(
+                lambda: self._driver_assist
+                .with_velocity_x(-hid.getLeftY() * self._max_speed)
+                .with_velocity_y(-hid.getLeftX() * self._max_speed)
+                .with_rotational_rate(-self._driver_controller.getRightX() * self._max_angular_rate)
+                .with_fallback(self._field_centric)
+                .with_change_target_pose(False)
+                .with_branch_side(DriverAssist.BranchSide.LEFT)
+            )
+            )
+        )
+
+        self._driver_controller.x().onTrue(self.drivetrain.runOnce(lambda: self.drivetrain.seed_field_centric()))
 
         self._setup_sysid_bindings(
             self._driver_controller, self.drivetrain,
